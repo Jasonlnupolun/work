9b5c2a2d05cef7917cba4250c13e5063234e2fda
forview <634004232@qq.com>
2016-12-22 17:51:24 +0800

    kun shan project

--- src/main/scala/com/scala/kanke/common/Constant.scala
+++ src/main/scala/com/scala/kanke/common/Constant.scala
@@ -10,15 +10,20 @@ import com.scala.kanke.dao.DaoImpl
   */
 object Constant {
 
-  val list = List[String]("tv","arts","film","anime")
+  val list = ConfigClass.classtypename
   val formatter = new java.text.DecimalFormat("#.000")
   val dao = new DaoImpl
   val tags = dao.querytags("")
   val regions = dao.queryOrgion("")
   val coordinate = tags++regions
   val mapGraph = list.map(x=>(x,
-    Knn.knnGraph(20,new Vectoriza(dao.findByType(x),coordinate).vectorizer())
+    Knn.knnGraph(1000,new Vectoriza(dao.findByType(x),coordinate).vectorizer())
     )).toMap
+
+
+  // tags 标签进行 混合推荐
+  val allGraph =
+    Knn.knnGraph(1000,new Vectoriza(dao.findAll,coordinate).vectorizer())
 
 
 }
---
+++ src/main/scala/com/scala/kanke/common/TagConfigClass.scala
@@ -0,0 +1,16 @@
+package com.scala.kanke.common
+
+import com.java.kanke.utils.PropertyUtil
+
+/**
+  * Created by Administrator on 2016/12/22.
+  */
+object TagConfigClass {
+
+
+  val all = PropertyUtil.get("all")
+  val tagDefaultVideo = PropertyUtil.get("tagDefaultVideo")
+  val prefix = PropertyUtil.get("prefixtags")
+  val useridhistory = PropertyUtil.get("useridhistory")
+
+}
