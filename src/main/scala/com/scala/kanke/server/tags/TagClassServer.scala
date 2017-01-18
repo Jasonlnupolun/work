bebf6a3e4045416898c5bc7916e1abc16452ecab
Jasonlnupolun <983068303@qq.com>
2017-01-18 21:43:39 +0800

    kun shan project

--- src/main/scala/com/scala/kanke/server/tags/TagClassServer.scala
+++ src/main/scala/com/scala/kanke/server/tags/TagClassServer.scala
@@ -17,7 +17,6 @@ import scala.collection.mutable.ArrayBuffer
 trait TagClassServer {
   def getOrDefault():List[Video]
   def getuserHisttory(userid:List[UserHistory]):List[FeatureBean]
-
 }
 
 class TagClassServerImpl extends TagClassServer{
@@ -39,7 +38,7 @@ class TagClassServerImpl extends TagClassServer{
     //2.根据用户的历史进行聚类
     val canopys = doCanopy(userVideolist)
     //3.根据聚类的中心向量进行knn  并进行去重复
-    val knnResult = getKnnResult(canopys,historyIds).sortWith(_.clusterweight>_.clusterweight).take(10)
+    val knnResult = getKnnResult(canopys,historyIds).sortWith(_.clusterweight>_.clusterweight).take(100)
     //4.遍历所有的簇  获取影片簇的标签  设置该簇的权重和影片的数量
     var tagTemp = scala.collection.mutable.Set[String]() // 使簇内影片不重复
     var labelMap = new java.util.LinkedHashMap[String,String]() // 使簇内影片不重复
