e942fb65d1e0e117c69da94257f927fbdafa4e1e
forview <634004232@qq.com>
2017-02-07 13:52:32 +0800

    kun shan project

---
+++ src/main/java/com/java/kanke/utils/kmeans/Test2.java
@@ -0,0 +1,20 @@
+package com.java.kanke.utils.kmeans;
+
+/**
+ * Created by Administrator on 2016/12/29.
+ */
+public class Test2 {
+
+    public static void main(String[] args) {
+        int K = 25;
+        String[] dataPaths = new String[]{"breast-cancer.data", "segment.data","housing.data", "meta.data"};
+        for (String path : dataPaths) {
+            DataSet dataset = new DataSet(path);
+
+            Evaluation eva = new Evaluation(dataset, "Kmeans");
+            eva.evaluateClustering(K);
+            // print mean and standard deviation of accuracy
+            System.out.println("Dataset:" + path + ", distance:" + eva.getClusterDist());
+        }
+    }
+}
